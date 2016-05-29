6cff74000901a068ea16061c365d4238f6674130
Mike Schwörer <mailport@mikescher.de>
2016-05-29 19:44:15 +0200

    started with TOML config parsing

--- main.go
+++ main.go
@@ -3,8 +3,38 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/BurntSushi/toml"
 )
 
+type GGMConfig struct {
+	TemporaryPath string
+	Remote        []GGMirror
+}
+
+type GGMirror struct {
+	Source string
+	Target string
+
+	Branches []string
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	var config GGMConfig
+	if _, err := toml.DecodeFile("config.toml", &config); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("GoGitMirror path: " + config.TemporaryPath)
+
+	for _, cfgRemote := range config.Remote {
+		fmt.Println(cfgRemote.Source + "  ->  " + cfgRemote.Target)
+
+		for _, cfgBranch := range cfgRemote.Branches {
+			fmt.Println("    | " + cfgBranch)
+		}
+
+		fmt.Println("")
+	}
 }
